e4855e238666b43da9f7169c4d753d9e105c4dd2
Hugo <hugoarielsanchez8@gmail.com>
2022-07-13 22:12:31 -0300

    Agregado Home y Login

--- ProInt_HugoSanchez/FrontEnd/src/app/app.module.ts
+++ ProInt_HugoSanchez/FrontEnd/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import{ HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,9 @@ import{ HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
