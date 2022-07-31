a3f3e9fccfe41de1d5e08d24b9e1bc7dfb2fca03
Hugo <hugoarielsanchez8@gmail.com>
2022-07-31 02:44:09 -0300

    Ajustes en el Back

--- src/main/java/com/portfolio/BackEnd/Controller/CEducacion.java
+++ src/main/java/com/portfolio/BackEnd/Controller/CEducacion.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliohsanchez.web.app")
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
--- src/main/java/com/portfolio/BackEnd/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/BackEnd/Controller/CExperiencia.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliohsanchez.web.app")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
